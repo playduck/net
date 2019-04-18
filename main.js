3bf1c1bcdd5ea275bcbe21a418e687389dccefea
playduck <robin@Robins-MBP.fritz.box>
2019-04-18 21:28:20 +0200

    style updates

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 /* by ROBIN PRILLWITZ 2019  */
 /* jshint esversion: 6 */
 
-document.addEventListener('DOMContentLoaded', function() {
-    for(let i = 0; i < 9; i++) {
-        addItem(i);
+const themes = [
+    {
+        main_bg: "#404040",
+        main_text: "#F2F2F2",
+        main_accent: "#F29877",
+        sec_bg: "#0D0D0D"
+    },
+    {
+        main_bg: "#262626",
+        main_text: "#93A2BF",
+        main_accent: "#E4EDF2",
+        sec_bg: "#363B59"
     }
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    let t = themes[Math.round(Math.random() * themes.length)];
+    document.getElementById("root").innerHTML = `
+    :root {
+        --main-bg: ${t.main_bg};
+        --main-text: ${t.main_text};
+        --main-accent: ${t.main_accent};
+        --sec-bg: ${t.sec_bg};
+    }`;
 }, false);
 
-function addItem(i)  {
-    let container = document.createElement("div");
-        container.classList.add("grid-item");
-        container.classList.add("hide");
-        container.style.backgroundImage = `url(./img/projects/${i}.jpg)`;
-/*     let img = document.createElement("img");
-        img.src = `./img/projects/${i}.jpg`;
-        img.alt = "image";
-    container.appendChild(img); */
-    document.getElementById("images").appendChild(container);
-    setTimeout( () => {
-        container.classList.remove("hide");
-    }, i*80);
-}
