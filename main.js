4a56b9eb3fa3e8ca591e05f38d8a5ace253556af
playduck <robin@Robins-MBP.fritz.box>
2019-04-19 15:26:05 +0200

    added video avatar

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 /* by ROBIN PRILLWITZ 2019  */
 /* jshint esversion: 6 */
 
-const themes = [
-    {
-        main_bg: "#404040",
-        main_text: "#F2F2F2",
-        main_accent: "#F29877",
-        sec_bg: "#0D0D0D"
-    },
-    {
-        main_bg: "#262626",
-        main_text: "#93A2BF",
-        main_accent: "#E4EDF2",
-        sec_bg: "#363B59"
-    }
-];
-
 document.addEventListener('DOMContentLoaded', function() {
-    let t = themes[Math.round(Math.random() * themes.length)];
-    document.getElementById("root").innerHTML = `
-    :root {
-        --main-bg: ${t.main_bg};
-        --main-text: ${t.main_text};
-        --main-accent: ${t.main_accent};
-        --sec-bg: ${t.sec_bg};
-    }`;
+    document.getElementById("avatar").play();
 }, false);
 
