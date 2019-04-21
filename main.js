2db3029e1485c15cb5303874e088fa4df4d5cc31
playduck <robin@Robins-MBP.fritz.box>
2019-04-21 17:38:33 +0200

    style updates and infobar

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 /* by ROBIN PRILLWITZ 2019  */
 /* jshint esversion: 6 */
 
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout( () => {
+document.addEventListener('DOMContentLoaded', function () {
+
+    setTimeout(() => {
         document.getElementById("avatar").play();
 
-        // document.querySelector('#submit').scrollIntoView({ 
-        //     behavior: 'smooth'
-        // });
+        if (location.search) {
+            if (location.search.includes("mailsend", 0)) {
+                document.getElementById("bar").classList.add("success");
+            } else if (location.search.includes("mailerr", 0)) {
+                document.getElementById("bar").classList.add("err");
+            }
+            document.getElementById("bar").classList.add("show");
+            setTimeout(() => {
+                document.getElementById("bar").classList.remove("show");
+            }, 4500);
+        }
+
     }, 200);
 
-    const items = [...document.querySelector("header nav ul").children];
-    items.forEach( (e) => {
+/*     const items = [...document.querySelector("header nav ul").children];
+    items.forEach((e) => {
         e.addEventListener("click", () => {
-            // document.getElementById(e.getAttribute("target")).scrollIntoView({ 
-            //     behavior: 'smooth'
-            // });
             let body = document.body.getBoundingClientRect();
             let el = document.getElementById(e.getAttribute("target")).getBoundingClientRect();
-            let top = Math.round( el.top - body.top -55 );
-            console.log(top);
             window.scrollTo({
-                left: 0, 
-                top: top,
+                left: 0,
+                top: Math.round(el.top - body.top - 55),
                 behavior: "smooth"
             });
         });
-    });
+    }); */
 }, false);
 
 function updateSubmit() {
@@ -40,9 +45,15 @@ function validateForm() {
     let e = document.forms.send.email.value;
     let t = document.forms.send.msg.value;
 
-    if(n == undefined || n.trim() == "")    {valid = false; }
-    if(e == undefined || e.trim() == "" || !validateEmail(e))    {valid = false; }
-    if(t == undefined || t.trim() == "")    {valid = false; }
+    if (n == undefined || n.trim() == "") {
+        valid = false;
+    }
+    if (e == undefined || e.trim() == "" || !validateEmail(e)) {
+        valid = false;
+    }
+    if (t == undefined || t.trim() == "") {
+        valid = false;
+    }
 
     return valid;
 }
