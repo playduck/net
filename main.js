58dc20751093a7696469b305e3039d0b575d4921
playduck <robin@Robins-MBP.fritz.box>
2019-04-21 18:41:45 +0200

    added hero fade

--- main.js
+++ main.js
@@ -3,6 +3,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+    window.addEventListener("scroll", (e) => {
+        let delta = (Math.round(window.scrollY + 50) - Math.round(parseFloat(window.getComputedStyle(document.querySelector(".hero-img")).height)));
+        document.querySelector(".fixed").style.opacity = Math.max(Math.min(map_range(delta, -300, 50, 1, 0),1) , 0);
+
+    });
+
     setTimeout(() => {
         document.getElementById("avatar").play();
 
@@ -20,18 +26,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }, 200);
 
-/*     const items = [...document.querySelector("header nav ul").children];
-    items.forEach((e) => {
-        e.addEventListener("click", () => {
-            let body = document.body.getBoundingClientRect();
-            let el = document.getElementById(e.getAttribute("target")).getBoundingClientRect();
-            window.scrollTo({
-                left: 0,
-                top: Math.round(el.top - body.top - 55),
-                behavior: "smooth"
+    /*     const items = [...document.querySelector("header nav ul").children];
+        items.forEach((e) => {
+            e.addEventListener("click", () => {
+                let body = document.body.getBoundingClientRect();
+                let el = document.getElementById(e.getAttribute("target")).getBoundingClientRect();
+                window.scrollTo({
+                    left: 0,
+                    top: Math.round(el.top - body.top - 55),
+                    behavior: "smooth"
+                });
             });
-        });
-    }); */
+        }); */
 }, false);
 
 function updateSubmit() {
@@ -61,4 +67,8 @@ function validateForm() {
 function validateEmail(email) {
     let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
+}
+
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
