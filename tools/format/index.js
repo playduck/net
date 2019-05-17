2faa16e8fa3498e2981c93193e7078b69a5e8ec7
playduck <robin@Robins-MBP.fritz.box>
2019-05-17 23:04:25 +0200

    added table support

--- tools/format/index.js
+++ tools/format/index.js
@@ -17,31 +17,49 @@ document.addEventListener('DOMContentLoaded', function () {
     // };
 
     document.getElementById("in").oninput = function() {
-        let text = document.getElementById("in").value;
-        let sp = text.split('\n\n');
-        console.log(sp);
-        
-        let name = sp[0];
+       doUpdate();
+    };
 
-        let att = "";
-        let att_sp = sp[1].split("\n");
-        let att_reg = /(^.*:)(\n?)(.*)/;
+    document.getElementById("tabelle").onchange = function() {
+        doUpdate();
+    };
+});
 
+function doUpdate() {
+    let text = document.getElementById("in").value;
+    let sp = text.split('\n\n');
+    // console.log(sp);
+    
+    let name = sp[0];
+
+    let att = "";
+    let att_sp = sp[1].split("\n");
+    let att_reg = /(^.*:)(\n?)(.*)/;
+
+    if(document.getElementById("tabelle").checked !== true) {
         for(let i = 0; i < att_sp.length; i++)  {
             if( att_sp[i] ) {
                 let a = att_reg.exec(att_sp[i]);
-                console.log(a);
+                // console.log(a);
                 att += `<b>${a[1]}</b>${a[2]}${a[3]} <br>`;
             }
         }
-        
-        let main = "\n";
-        for(let i = 2; i < sp.length; i++)  {
-            main += "<br>" + sp[i];
-        }
+    }   else    {
+        for(let i = 0; i < att_sp.length; i+=2)  {
+            console.log( att_sp[i] );
+            console.log( att_sp[i+1] );
 
-        document.getElementById("out-name").innerHTML = name;
-        document.getElementById("out-att").innerHTML = att;
-        document.getElementById("out-text").innerHTML = main;
-    };
-});
+            att += `<b>${att_sp[i]}</b> ${att_sp[i+1]} <br>`;
+        }
+    } 
+    
+    let main = "\n";
+    for(let i = 2; i < sp.length; i++)  {
+        main += "<br>" + sp[i];
+    }
+
+    document.getElementById("out-name").innerHTML = name;
+    document.getElementById("out-att").innerHTML = att;
+    document.getElementById("out-text").innerHTML = main;
+}
+
