ed72b15b51d7272fdcb49b73eeee130759079203
robin <robin.prillwitz@gmail.com>
2021-04-07 19:15:08 +0200

    updated for 2021

--- main.js
+++ main.js
@@ -1,83 +1,48 @@
-/* by ROBIN PRILLWITZ 2019  */
-/* jshint esversion: 6 */
+/* by ROBIN PRILLWITZ 2021  */
+/* jshint esversion: 9 */
 
-document.addEventListener('DOMContentLoaded', function () {
+const main_bg = "#0f1010"
+const main_text = "rgb(239, 239, 244)"
+const sec_bg = "rgb(42, 45, 46)"
+    // const tint = "rgb(16, 63, 41)"
 
-    window.addEventListener("resize", updateBig);
-    updateBig();
+function setStyle(tint) {
+    const rootEl = document.getElementById("root");
+    rootEl.innerHTML = `
+    :root {
+        --main-bg: ${main_bg}!important;
+        --main-text: ${main_text}!important;
+        --main-accent: ${tint}!important;
+        --sec-bg: ${sec_bg}!important;
+        --tint: ${tint}!important;
+    }`;
+}
 
-    window.addEventListener("scroll", (e) => {
-        document.getElementById("big-name").style = `transform: translate(-50%, ${window.scrollY / 1.5}px)!important;`;
-    });
+function timeToColor() {
+    const d = new Date();
 
-    setTimeout(() => {
-        document.getElementById("avatar").play();
+    let hours = d.getHours().toString();
+    let minutes = d.getMinutes().toString();
+    let seconds = d.getSeconds().toString();
 
-        if (location.search) {
-            if (location.search.includes("mailsend", 0)) {
-                document.getElementById("bar").classList.add("success");
-            } else if (location.search.includes("mailerr", 0)) {
-                document.getElementById("bar").classList.add("err");
-            }
-            document.getElementById("bar").classList.add("show");
-            setTimeout(() => {
-                document.getElementById("bar").classList.remove("show");
-            }, 4500);
-        }
+    if (hours <= 9) { hours = '0' + hours };
+    if (minutes <= 9) { minutes = '0' + minutes };
+    if (seconds <= 9) { seconds = '0' + seconds };
 
-    }, 200);
+    console.log(hours, minutes, seconds)
 
-    /*     const items = [...document.querySelector("header nav ul").children];
-        items.forEach((e) => {
-            e.addEventListener("click", () => {
-                let body = document.body.getBoundingClientRect();
-                let el = document.getElementById(e.getAttribute("target")).getBoundingClientRect();
-                window.scrollTo({
-                    left: 0,
-                    top: Math.round(el.top - body.top - 55),
-                    behavior: "smooth"
-                });
-            });
-        }); */
+    return `#${hours}${minutes}${seconds}`;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("There's no easter eggs here")
+
+    setStyle("rgb(12, 10, 16)");
+
+    setInterval(() => {
+        const color = timeToColor();
+        console.log(color)
+        setStyle(color);
+    }, 1000);
+
 }, false);
-
-function updateSubmit() {
-    document.getElementById("submit").disabled = !validateForm();
-}
-
-function validateForm() {
-    let valid = true;
-
-    let n = document.forms.send.name.value;
-    let e = document.forms.send.email.value;
-    let t = document.forms.send.msg.value;
-
-    if (n == undefined || n.trim() == "") {
-        valid = false;
-    }
-    if (e == undefined || e.trim() == "" || !validateEmail(e)) {
-        valid = false;
-    }
-    if (t == undefined || t.trim() == "") {
-        valid = false;
-    }
-
-    return valid;
-}
-
-function validateEmail(email) {
-    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(String(email).toLowerCase());
-}
-
-function map_range(value, low1, high1, low2, high2) {
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
-
-function updateBig()    {
-    if(window.innerWidth <= 1550)   {
-        document.getElementById("big-name").innerText = "R.";
-    }   else    {
-        document.getElementById("big-name").innerText = "robin.";
-    }
-}
