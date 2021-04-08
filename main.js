3c84fd326aacdca76a2245351ff17aa939bc1dd3
robin <robin.prillwitz@gmail.com>
2021-04-08 07:49:00 +0200

    added time display and various style updates

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 /* by ROBIN PRILLWITZ 2021  */
 /* jshint esversion: 9 */
 
-const main_bg = "#0f1010"
-const main_text = "rgb(239, 239, 244)"
-const sec_bg = "rgb(42, 45, 46)"
-    // const tint = "rgb(16, 63, 41)"
+const main_bg = "#0f1010";
+const main_text = "rgb(239, 239, 244)";
+const sec_bg = "rgb(42, 45, 46)";
+
+const color_el = document.getElementById("color");
+const timezone_el = document.getElementById("timezone");
 
 function setStyle(tint) {
     const rootEl = document.getElementById("root");
@@ -29,19 +31,25 @@ function timeToColor() {
     if (minutes <= 9) { minutes = '0' + minutes };
     if (seconds <= 9) { seconds = '0' + seconds };
 
-    console.log(hours, minutes, seconds)
+    // console.log(hours, minutes, seconds);
 
-    return `#${hours}${minutes}${seconds}`;
+    const color = `#${hours}${minutes}${seconds}`
+
+    color_el.innerHTML = color;
+    timezone_el.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    return color;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log("There's no easter eggs here")
+    console.log("There's no easter eggs here");
 
     setStyle("rgb(12, 10, 16)");
 
     setInterval(() => {
         const color = timeToColor();
-        console.log(color)
+
+        // console.log(color);
         setStyle(color);
     }, 1000);
 
